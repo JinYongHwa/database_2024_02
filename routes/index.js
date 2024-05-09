bc7a6d0b1c42ded829c78b82dd8995f423a0323c
jinyonghwa <jinyonghwa>
2024-05-09 17:08:05 +0900

    0509

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  var [rows] = await connection.query("select * from board")
+  var [rows] = await connection.query("select * from board order by writeDate desc")
   res.render('index', { boards: rows });
 });
 
 router.get("/write", function (req, res) {
   res.render("write")
 })
+
+router.post("/board/write", async function (req, res) {
+  console.log(req.body)
+  await connection.query("insert into board(title,writer,body) values(?,?,?)",
+    [req.body.title, req.body.writer, req.body.body])
+
+  res.redirect("/")
+})
+
+router.get("/view/:no", async function (req, res) {
+  var no = req.params.no
+
+  var [rows] = await connection.query("select * from board where no=?", [no])
+  var board = rows[0]
+  board.hits++
+  await connection.query("update board set hits=? where no=?", [board.hits, no])
+
+  res.render("view", { board: board })
+
+})
+router.get("/delete/:no", async function (req, res) {
+  var no = req.params.no
+  await connection.query("delete from board where no=?", [no])
+  res.redirect("/")
+})
+
 
 module.exports = router;
